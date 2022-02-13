94787f5512f14db537e05fb5905e534c0d0a64b8
Nastya <s4astliva.ya1@gmail.com>
2022-02-13 11:15:41 +0200

    Исправила сезоны

--- hw8/season.py
+++ hw8/season.py
@@ -1,18 +1,15 @@
 _month = int(input("Введите номер месяца от 1 до 12: "))
 
-if _month > 12 or _month == 0:
-    print("Введите правильное число!")
-    exit()
-
 def season(month):
-    if _month == 12 or _month < 3:
+
+    if month == 12 or 1 <= month < 3:
         return "Зима"
-    elif _month == 3 or _month < 6:
+    elif 3 <= month <= 5:
         return "Весна"
-    elif _month == 6 or _month < 9:
+    elif 6 <= month <= 8:
         return "Лето"
-    else:
+    elif 9 <= month <= 11:
         return "Осень"
+    return ("Введите правильное число!")
 
 print(season(_month))
-
