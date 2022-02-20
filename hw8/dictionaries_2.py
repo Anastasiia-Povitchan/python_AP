2a06b5170ef195b48f532c093d0c7823f2eec2b1
Nastya <s4astliva.ya1@gmail.com>
2022-02-20 14:11:35 +0200

    слова

---
+++ hw8/dictionaries_2.py
@@ -0,0 +1,27 @@
+text = input("Введите текст: ").split()
+word = {}
+
+for i in text:
+    word[i] = word.get(i, 0) + 1
+
+m = max(word.values())
+
+s = {}
+
+for w in word:
+    if word[w] == m:
+        # print(word)
+        s = w
+
+print("Слово, которое в этом тексте встречается чаще всего - ", s)
+
+
+
+
+
+
+
+
+
+
+
